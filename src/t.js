f8a2b5e109a8a201d8e3cca5234fe17255dd6fd9
Pedro Ramos Paraiso <03ramos011@gmail.com>
2024-11-27 21:55:21 -0300

    first commit

---
+++ src/t.js
@@ -0,0 +1,42 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABIBLIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+  };
+  const player2 = {
+    NOME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABIBLIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+  };
+  const player3 = {
+    NOME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABIBLIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+  };
+  const player4 = {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABIBLIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  };
+  const player5 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABIBLIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+  };
+  const player6 = {
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABIBLIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  };
